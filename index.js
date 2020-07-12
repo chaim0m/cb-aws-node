bd13fe5831c99dcee5af168cca5f2db049c1e93e
Chaim <cfinson@emergingmed.com>
2020-07-12 22:34:16 +0300

    split logic into 2 functions for testing and separation of concerns

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ let operations = require("./queries/operations")
 
 console.log("Starting index.js")
 //running this file will return these 3 users with the time of each call (on same object) and average
-
+operations.getUsersByIds([1,11,111]).then((users) => {  //this function split into 2 functions "getUsersByIds" and "withAverage" mainly for testing purposes
+    return withAverage(users)
+})
